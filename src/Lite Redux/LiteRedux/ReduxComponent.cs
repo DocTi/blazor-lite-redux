54b700e9035b5c756bb90f1dd595b1ab2b90708f
Maxim <sirotkin.maxim.dev@yandex.ru>
2020-02-11 23:45:12 +0300

    redux component

---
+++ src/Lite Redux/LiteRedux/ReduxComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace LiteRedux
+{
+    public class ReduxComponent<TState> : ComponentBase, IDisposable
+    {
+        [Inject]
+        protected IState<TState> State { get; set; }
+
+        [Inject]
+        protected IStore Store { get; set; }
+
+        protected override void OnInitialized()
+        {
+            State.Subscribe(StateHasChanged);
+        }
+
+        public virtual void Dispose()
+        {
+            State.Unsubscribe(StateHasChanged);
+        }
+    }
+}
