4cb4359e1a5b7b6e628b2aa804c714efa69439b3
Maxim <sirotkin.maxim.dev@yandex.ru>
2020-02-11 23:45:01 +0300

    dependency scanner

---
+++ src/Lite Redux/LiteRedux/ReduxBuilder.cs
@@ -0,0 +1,27 @@
+﻿using LiteRedux;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class ReduxBuilder
+    {
+        private readonly IServiceCollection services;
+
+        internal ReduxBuilder(IServiceCollection services)
+        {
+            this.services = services;
+        }
+
+        public ReduxBuilder AddState<TState>() where TState : class, new()
+        {
+            services.AddScoped<StateMapper<TState>>();
+            services.AddScoped<IStateMapper>(provider => provider.GetService<StateMapper<TState>>());
+            services.AddScoped<IStateMapper<TState>>(provider => provider.GetService<StateMapper<TState>>());
+
+            //services.AddScoped<IReducer<TState>, TImlementation>();
+
+            //services.AddScoped<ITrigger, TImlementation>();
+
+            return this;
+        }
+    }
+}
